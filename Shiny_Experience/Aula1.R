6d280da2d3bc8d102029a748435ec4c7cd2b815d
JoaoVictorMagalhaesSouza <joao.souza5@ufv.br>
2021-03-28 19:08:54 -0300

    Tópicos Iniciais

---
+++ Shiny_Experience/Aula1.R
@@ -0,0 +1,19 @@
+seno <- sin(45)
+coseno <- cos(45)
+seno
+coseno
+
+
+#Vetores
+
+#c = concatenar
+cidades <- c("BH","Rio","São Paulo")
+cidades
+#length = tamanho
+length(cidades)
+#min e max = mínimo e máximo
+numeros <- c(500,2,3,600,5)
+min(numeros)
+max(numeros)
+#sum = soma dos numeros
+sum(numeros)
